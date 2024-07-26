c1565321352b13a40bd329513362667d0b6f7936
Osorio Brenda <patituchi59@gmail.com>
2024-07-26 11:42:18 -0300

    main

--- src/main.py
+++ src/main.py
@@ -1,1 +1,18 @@
 import pygame
+from pantallas import *
+
+pygame.init()
+pygame.font.init()
+corriendo_juego = True
+def main_principal():
+    global dibujar_menu
+    while corriendo_juego:
+        clock = pygame.time.Clock()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                corriendo_juego = False
+
+        dibujar_menu(imagen_fondo)
+        pygame.display.flip()
+        clock.tick(FPS)
+main_principal()
